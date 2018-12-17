3dab7499aba65d178dbc023e497838e73ef696f2
Takeshi Akuhara <akuhara@eri.u-tokyo.ac.jp>
2018-12-17 16:33:15 +0900

    add rf_inv

---
+++ src/rf_inv.f90
@@ -0,0 +1,26 @@
+program main
+  use params
+  implicit none 
+  include "mpif.h"
+  integer :: nproc, rank, ierr
+  logical :: verb
+
+  ! Initialize MPI 
+  call mpi_init(ierr)
+  call mpi_comm_size(MPI_COMM_WORLD, nproc, ierr)
+  call mpi_comm_rank(MPI_COMM_WORLD, rank,  ierr)
+  if (nproc < 2) then
+     write(0,*)"ERROR: at least 2 processor is necesarry!"
+     call mpi_finalize(ierr)
+     stop
+  end if
+  
+  ! read parameters from file
+  verb = .false.
+  if (rank == 0) verb = .true.  
+  call get_params(verb, "params.in") ! if rank == 0 -> verbose
+  
+
+
+  stop
+end program main
