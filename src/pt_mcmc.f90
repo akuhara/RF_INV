93550d08fd6fa7bd604b4fe00931ffeb86c913c7
Takeshi Akuhara <akuhara@eri.u-tokyo.ac.jp>
2019-01-30 10:57:49 +0900

    layout change

--- src/pt_mcmc.f90
+++ src/pt_mcmc.f90
@@ -35,9 +35,201 @@ module pt_mcmc
   real(8) :: dbin_vp, dbin_vs, dbin_z, dbin_amp, dbin_sig
 
   public init_pt_mcmc
-  private judge_mcmc, judge_pt
+  private judge_mcmc, judge_pt, mcmc
 contains
   
+  !---------------------------------------------------------------------
+  subroutine mcmc(iter, ichain, temp)
+    use mt19937
+    use model
+    use likelihood
+    use forward
+    use math
+    implicit none 
+    integer, intent(in) :: iter, ichain
+    real(8), intent(in) :: temp
+    real(8) :: prop_log_likelihood, prop_rft(nfft, ntrc)
+    real(8) :: prop_dvp(k_max), prop_dvs(k_max), prop_z(k_max)
+    real(8) :: prop_sig(ntrc), tmpz
+    integer :: prop_k
+    integer :: itype, itarget, ilay, ibin, iz1, iz2, itrc, iz
+    integer :: ivp, ivs, it
+    logical :: null_flag, yn, fwd_flag
+    integer :: nlay
+    real(8) :: alpha(nlay_max), beta(nlay_max)
+    real(8) :: h(nlay_max), rho(nlay_max)
+    
+    prop_k = k(ichain)
+    prop_dvp(1:k_max)   = dvp(1:k_max, ichain)
+    prop_dvs(1:k_max)   = dvs(1:k_max, ichain)
+    prop_z(1:k_max - 1) = z(1:k_max - 1, ichain)
+    prop_sig(1:ntrc)    = sig(1:ntrc, ichain)
+
+    null_flag = .false.
+
+    ! Select proposal type
+    itype = int(grnd() * ntype) + 1
+
+    if (itype == 1) then
+       ! Birth proposal
+       prop_k = prop_k + 1
+       if (prop_k < k_max) then
+          prop_dvp(prop_k) = dvp_min + grnd() * (dvp_max - dvp_min)
+          prop_dvs(prop_k) = dvs_min + grnd() * (dvs_max - dvs_min)
+          prop_z(prop_k)   = z_min   + grnd() * (z_max   - z_min)
+       else 
+          null_flag = .true.
+       end if
+    else if (itype == 2) then
+       ! Death proposal
+       prop_k = prop_k - 1
+       if (prop_k >= k_min) then
+          itarget = int(grnd() * (prop_k + 1)) + 1
+          do ilay = itarget, prop_k
+             prop_dvp(ilay) = dvp(ilay + 1, ichain)
+             prop_dvs(ilay) = dvs(ilay + 1, ichain)
+             prop_z(ilay)   = z(ilay + 1, ichain)
+          end do
+          prop_dvp(prop_k + 1) = 0.d0
+          prop_dvs(prop_k + 1) = 0.d0
+          prop_z(prop_k + 1)   = 0.d0
+       else
+          null_flag = .true.
+       end if
+    else if (itype == 3) then
+       ! Move interface
+       itarget = int(grnd() * prop_k) + 1
+       prop_z(itarget) = prop_z(itarget) + gauss() * dev_z
+       if (prop_z(itarget) < z_min .or. &
+            & prop_z(itarget) > z_max) then
+          null_flag = .true.
+       end if
+    else if (itype == 4) then
+       ! Perturb noise sigma
+       itarget = int(grnd() * ntrc) + 1
+       prop_sig(itarget) = prop_sig(itarget) + gauss() * dev_sig
+       if (prop_sig(itarget) < sig_min .or. &
+            & prop_sig(itarget) > sig_max) then
+          null_flag = .true.
+       end if
+    else if (itype == 5) then
+       ! Perturb dVs
+       itarget = int(grnd() * (prop_k + 1)) + 1
+       if (itarget == prop_k + 1) itarget = k_max
+       prop_dvs(itarget) = prop_dvs(itarget) + gauss() * dev_dvs
+       if (prop_dvs(itarget) < dvs_min .or. &
+            & prop_dvs(itarget) > dvs_max) then
+          null_flag = .true.
+       end if
+    else 
+       ! Perturb dVp
+       itarget = int(grnd() * (prop_k + 1)) + 1
+       if (itarget == prop_k + 1) itarget = k_max
+       prop_dvp(itarget) = prop_dvp(itarget) + gauss() * dev_dvp
+       if (prop_dvp(itarget) < dvp_min .or. &
+            & prop_dvp(itarget) > dvp_max) then
+          null_flag = .true.
+       end if
+    end if
+    
+    
+    ! evaluate proposed model
+    if (.not. null_flag) then
+       if (itype /= 4) then
+          fwd_flag = .true.
+       else
+          fwd_flag = .false.
+       end if
+       call calc_likelihood(ichain, fwd_flag, &
+            & prop_k, prop_z, prop_dvp, prop_dvs, &
+            & prop_sig, prop_log_likelihood, prop_rft)
+       call judge_mcmc(temp, log_likelihood(ichain), &
+            & prop_log_likelihood, yn)
+       if (yn) then
+          log_likelihood(ichain) = prop_log_likelihood
+          k(ichain)              = prop_k
+          dvp(1:k_max, ichain)   = prop_dvp(1:k_max)
+          dvs(1:k_max, ichain)   = prop_dvs(1:k_max)
+          z(1:k_max - 1, ichain) = prop_z(1:k_max - 1)
+          sig(1:ntrc, ichain)    = prop_sig(1:ntrc)
+          rft(1:nfft, 1:ntrc, ichain) = prop_rft(:,:)
+       end if
+    end if
+
+    
+    ! count proposal
+    if (temp <= 1.d0 + 1.0e-6) then
+       nprop(itype) = nprop(itype) + 1
+       if (yn) naccept(itype) = naccept(itype) + 1
+    end if
+
+    ! record sampled model
+    if (temp <= 1.d0 + 1.0e-6 .and. iter > nburn .and. &
+         & mod(iter - nburn, ncorr) == 0) then
+       
+       nmod = nmod + 1
+
+       ! # of layer interfaces
+       nk(k(ichain)) = nk(k(ichain)) + 1
+       
+       ! Noise sigma
+       do itrc = 1, ntrc
+          ibin = int((sig(itrc, ichain) - sig_min) / dbin_sig) + 1
+          nsig(ibin, itrc) = nsig(ibin, itrc) + 1
+       end do
+
+       ! Interface depth
+       do ilay = 1, k(ichain) - 1
+          ibin = int((z(ilay, ichain) - z_min) / dbin_z) + 1
+          nz(ibin) = nz(ibin) + 1
+       end do
+
+
+       ! V-z profile
+       call format_model(k(ichain), z(1:k_max-1, ichain), &
+            & dvp(1:k_max, ichain), dvs(1:k_max, ichain), &
+            & nlay, alpha, beta, rho, h)
+       tmpz = 0.d0
+       do ilay = 1, nlay
+          iz1 = int(tmpz / dbin_z) + 1
+          if (ilay < nlay) then
+             iz2 = int((tmpz + h(ilay)) / dbin_z) + 1
+          else
+             iz2 = nbin_z + 1
+          end if
+          ivp = int((alpha(ilay) - vp_min) / dbin_vp) + 1
+          ivs = int((beta(ilay) - vs_min) / dbin_vs) + 1
+          ivs = max(1, ivs) ! for ocean layer where beta(1) < vs_min
+          do iz = iz1, iz2 - 1
+             nvpz(iz, ivp) = nvpz(iz, ivp) + 1
+             nvsz(iz, ivs) = nvsz(iz, ivs) + 1
+          end do
+          tmpz = tmpz + h(ilay)
+       end do
+
+       ! RF trace
+       do itrc = 1, ntrc
+          do it = 1, nsmp
+             ibin = int((rft(it, itrc, ichain) - amp_min) &
+                  & / dbin_amp) + 1
+             if (ibin < 1) then
+                write(0,*)"Warning: RF amp. out of range"
+                ibin = 1
+             else if (ibin > nbin_amp) then
+                write(0,*)"Warning: RF amp. out of range"
+                ibin = nbin_amp
+             end if
+             namp(ibin, it, itrc) = namp(ibin, it, itrc) + 1
+          end do
+       end do
+    end if
+
+    
+    return 
+  end subroutine mcmc
+
+
+
   !---------------------------------------------------------------------
   
   subroutine init_pt_mcmc(verb)
@@ -230,196 +422,7 @@ contains
 
   !---------------------------------------------------------------------
 
-  subroutine mcmc(iter, ichain, temp)
-    use mt19937
-    use model
-    use likelihood
-    use forward
-    use math
-    implicit none 
-    integer, intent(in) :: iter, ichain
-    real(8), intent(in) :: temp
-    real(8) :: prop_log_likelihood, prop_rft(nfft, ntrc)
-    real(8) :: prop_dvp(k_max), prop_dvs(k_max), prop_z(k_max)
-    real(8) :: prop_sig(ntrc), tmpz
-    integer :: prop_k
-    integer :: itype, itarget, ilay, ibin, iz1, iz2, itrc, iz
-    integer :: ivp, ivs, it
-    logical :: null_flag, yn, fwd_flag
-    integer :: nlay
-    real(8) :: alpha(nlay_max), beta(nlay_max)
-    real(8) :: h(nlay_max), rho(nlay_max)
-    
-    prop_k = k(ichain)
-    prop_dvp(1:k_max)   = dvp(1:k_max, ichain)
-    prop_dvs(1:k_max)   = dvs(1:k_max, ichain)
-    prop_z(1:k_max - 1) = z(1:k_max - 1, ichain)
-    prop_sig(1:ntrc)    = sig(1:ntrc, ichain)
-
-    null_flag = .false.
-
-    ! Select proposal type
-    itype = int(grnd() * ntype) + 1
-
-    if (itype == 1) then
-       ! Birth proposal
-       prop_k = prop_k + 1
-       if (prop_k < k_max) then
-          prop_dvp(prop_k) = dvp_min + grnd() * (dvp_max - dvp_min)
-          prop_dvs(prop_k) = dvs_min + grnd() * (dvs_max - dvs_min)
-          prop_z(prop_k)   = z_min   + grnd() * (z_max   - z_min)
-       else 
-          null_flag = .true.
-       end if
-    else if (itype == 2) then
-       ! Death proposal
-       prop_k = prop_k - 1
-       if (prop_k >= k_min) then
-          itarget = int(grnd() * (prop_k + 1)) + 1
-          do ilay = itarget, prop_k
-             prop_dvp(ilay) = dvp(ilay + 1, ichain)
-             prop_dvs(ilay) = dvs(ilay + 1, ichain)
-             prop_z(ilay)   = z(ilay + 1, ichain)
-          end do
-          prop_dvp(prop_k + 1) = 0.d0
-          prop_dvs(prop_k + 1) = 0.d0
-          prop_z(prop_k + 1)   = 0.d0
-       else
-          null_flag = .true.
-       end if
-    else if (itype == 3) then
-       ! Move interface
-       itarget = int(grnd() * prop_k) + 1
-       prop_z(itarget) = prop_z(itarget) + gauss() * dev_z
-       if (prop_z(itarget) < z_min .or. &
-            & prop_z(itarget) > z_max) then
-          null_flag = .true.
-       end if
-    else if (itype == 4) then
-       ! Perturb noise sigma
-       itarget = int(grnd() * ntrc) + 1
-       prop_sig(itarget) = prop_sig(itarget) + gauss() * dev_sig
-       if (prop_sig(itarget) < sig_min .or. &
-            & prop_sig(itarget) > sig_max) then
-          null_flag = .true.
-       end if
-    else if (itype == 5) then
-       ! Perturb dVs
-       itarget = int(grnd() * (prop_k + 1)) + 1
-       if (itarget == prop_k + 1) itarget = k_max
-       prop_dvs(itarget) = prop_dvs(itarget) + gauss() * dev_dvs
-       if (prop_dvs(itarget) < dvs_min .or. &
-            & prop_dvs(itarget) > dvs_max) then
-          null_flag = .true.
-       end if
-    else 
-       ! Perturb dVp
-       itarget = int(grnd() * (prop_k + 1)) + 1
-       if (itarget == prop_k + 1) itarget = k_max
-       prop_dvp(itarget) = prop_dvp(itarget) + gauss() * dev_dvp
-       if (prop_dvp(itarget) < dvp_min .or. &
-            & prop_dvp(itarget) > dvp_max) then
-          null_flag = .true.
-       end if
-    end if
-    
-    
-    ! evaluate proposed model
-    if (.not. null_flag) then
-       if (itype /= 4) then
-          fwd_flag = .true.
-       else
-          fwd_flag = .false.
-       end if
-       call calc_likelihood(ichain, fwd_flag, &
-            & prop_k, prop_z, prop_dvp, prop_dvs, &
-            & prop_sig, prop_log_likelihood, prop_rft)
-       call judge_mcmc(temp, log_likelihood(ichain), &
-            & prop_log_likelihood, yn)
-       if (yn) then
-          log_likelihood(ichain) = prop_log_likelihood
-          k(ichain)              = prop_k
-          dvp(1:k_max, ichain)   = prop_dvp(1:k_max)
-          dvs(1:k_max, ichain)   = prop_dvs(1:k_max)
-          z(1:k_max - 1, ichain) = prop_z(1:k_max - 1)
-          sig(1:ntrc, ichain)    = prop_sig(1:ntrc)
-          rft(1:nfft, 1:ntrc, ichain) = prop_rft(:,:)
-       end if
-    end if
-
-    
-    ! count proposal
-    if (temp <= 1.d0 + 1.0e-6) then
-       nprop(itype) = nprop(itype) + 1
-       if (yn) naccept(itype) = naccept(itype) + 1
-    end if
-
-    ! record sampled model
-    if (temp <= 1.d0 + 1.0e-6 .and. iter > nburn .and. &
-         & mod(iter - nburn, ncorr) == 0) then
-       
-       nmod = nmod + 1
-
-       ! # of layer interfaces
-       nk(k(ichain)) = nk(k(ichain)) + 1
-       
-       ! Noise sigma
-       do itrc = 1, ntrc
-          ibin = int((sig(itrc, ichain) - sig_min) / dbin_sig) + 1
-          nsig(ibin, itrc) = nsig(ibin, itrc) + 1
-       end do
-
-       ! Interface depth
-       do ilay = 1, k(ichain) - 1
-          ibin = int((z(ilay, ichain) - z_min) / dbin_z) + 1
-          nz(ibin) = nz(ibin) + 1
-       end do
-
-
-       ! V-z profile
-       call format_model(k(ichain), z(1:k_max-1, ichain), &
-            & dvp(1:k_max, ichain), dvs(1:k_max, ichain), &
-            & nlay, alpha, beta, rho, h)
-       tmpz = 0.d0
-       do ilay = 1, nlay
-          iz1 = int(tmpz / dbin_z) + 1
-          if (ilay < nlay) then
-             iz2 = int((tmpz + h(ilay)) / dbin_z) + 1
-          else
-             iz2 = nbin_z + 1
-          end if
-          ivp = int((alpha(ilay) - vp_min) / dbin_vp) + 1
-          ivs = int((beta(ilay) - vs_min) / dbin_vs) + 1
-          ivs = max(1, ivs) ! for ocean layer where beta(1) < vs_min
-          do iz = iz1, iz2 - 1
-             nvpz(iz, ivp) = nvpz(iz, ivp) + 1
-             nvsz(iz, ivs) = nvsz(iz, ivs) + 1
-          end do
-          tmpz = tmpz + h(ilay)
-       end do
-
-       ! RF trace
-       do itrc = 1, ntrc
-          do it = 1, nsmp
-             ibin = int((rft(it, itrc, ichain) - amp_min) &
-                  & / dbin_amp) + 1
-             if (ibin < 1) then
-                write(0,*)"Warning: RF amp. out of range"
-                ibin = 1
-             else if (ibin > nbin_amp) then
-                write(0,*)"Warning: RF amp. out of range"
-                ibin = nbin_amp
-             end if
-             namp(ibin, it, itrc) = namp(ibin, it, itrc) + 1
-          end do
-       end do
-    end if
-
-    
-    return 
-  end subroutine mcmc
-
-  !---------------------------------------------------------------------
+ 
   subroutine judge_mcmc(temp, log_lklh1, log_lklh2, yn)
     use mt19937
     implicit none
